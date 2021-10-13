787dbb0e6808bdc76d3ab392e430761b4f1478c5
Michel Breyer <michel.p.breyer@gmail.com>
2021-10-13 15:44:45 +0200

    Add evaluation script

---
+++ scripts/run.sh
@@ -0,0 +1,4 @@
+python3 scripts/run.py initial-view --runs $1 --logdir $2 --seed 1
+python3 scripts/run.py top-view --runs $1 --logdir $2 --seed 1
+python3 scripts/run.py top-trajectory --runs $1 --logdir $2 --seed 1
+python3 scripts/run.py nbv-grasp --runs $1 --logdir $2 --seed 1
