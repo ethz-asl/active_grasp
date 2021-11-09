1dd6f7875e40ac01ebbd54d3ab6c302f74452e08
Michel Breyer <michel.p.breyer@gmail.com>
2021-11-09 11:48:18 +0100

    Add script to test the simulation

---
+++ test/test_sim_scene.py
@@ -0,0 +1,13 @@
+from active_grasp.simulation import Simulation
+
+
+def main():
+    gui = True
+    scene_id = "random"
+    vgn_path = "../vgn/assets/models/vgn_conv.pth"
+    sim = Simulation(gui, scene_id, vgn_path)
+    bbox = sim.reset()
+
+
+if __name__ == "__main__":
+    main()
