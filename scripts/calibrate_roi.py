e7aa81fed3086a44f01bbf536ad8e082403f18ae
Michel Breyer <10465414+mbreyer@users.noreply.github.com>
2021-12-03 11:49:50 +0100

    Minor

--- scripts/calibrate_roi.py
+++ scripts/calibrate_roi.py
@@ -8,7 +8,7 @@ def main():
     rospy.init_node("calibrate_roi")
     tf.init()
     T_base_roi = tf.lookup("panda_link0", "tag_0")
-    np.savetxt("cfg/T_base_tag.txt", T_base_roi.as_matrix())
+    np.savetxt("cfg/hw/T_base_tag.txt", T_base_roi.as_matrix())
 
 
 if __name__ == "__main__":
